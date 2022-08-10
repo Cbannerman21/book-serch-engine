ebc9f9ca80a1cbdf3004631ee090cb5e32c36c04
Cbannerman <cbannerman21@gmail.com>
2022-08-10 11:26:55 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { 
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Building our GraphQL API endpoint
+const httpLink = createHttpLink({
+  eri: '/graphql',
+});
+
+//Building request middleware that will attach the JWT token to every request as an 'authorization' header
+const authLink = setContext((_, { headers }) => {
+  // get the auth token from local storage
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so that httpLink can read them
+  return { 
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }, 
+  };
+});
+
+const client = new ApolloClient({
+  // set up our client to execute `authLink` middleware prior to making a request to our GraphQL API
+  link: authLink.contact(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Switch>
@@ -16,6 +49,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
